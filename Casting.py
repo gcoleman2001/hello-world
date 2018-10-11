ec7f39bcf0f9d70b69d28eb5b13c1179085d3e69
gcoleman2001 <gcoleman2001@users.noreply.github.com>
2018-10-11 11:19:45 +0100

    Tester Upload

---
+++ Casting.py
@@ -0,0 +1,23 @@
+# Integers
+
+x = int(1)   	# x will be 1
+y = int(2.8) 	# y will be 2
+z = int("3") 	# z will be 3
+
+print(str(x) + ":" + str(y) + ":" + str(z))
+
+# floats
+
+x = float(1)     	# x will be 1.0
+y = float(2.8)   	# y will be 2.8
+z = float("3")   	# z will be 3.0
+w = float("4.2") 	# w will be 4.2
+
+print(str(x) + ":" + str(y) + ":" + str(z))
+
+# Strings
+x = str("s1") 	# x will be 's1'
+y = str(2)    	# y will be '2'
+z = str(3.0)  	# z will be '3.0'
+
+print(str(x) + ":" + str(y) + ":" + str(z))
